0974676b9082e497590477d905b2ed93223d4839
Paul Fitzgerald <paulfitz99@gmail.com>
2018-01-12 22:31:52 +0000

    update loading

--- components/Loading.js
+++ components/Loading.js
@@ -3,17 +3,17 @@ import { View, ActivityIndicator } from 'react-native';
 import styled from 'styled-components/native';
 
 const Loading = () => {
-	return(
-		<LoadingView>
-			<ActivityIndicator size="large" color="rgb(60, 0, 60)" />
-		</LoadingView>
-	);
+  return(
+    <LoadingView>
+      <ActivityIndicator size="large" color="rgb(60, 0, 60)" />
+    </LoadingView>
+  );
 }
 
 const LoadingView = styled.View`
-	justify-content: center;
-	align-items: center;
-	flex: 1;
+  justify-content: center;
+  align-items: center;
+  flex: 1;
 `;
 
 export default Loading;
