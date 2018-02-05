b4f79c4225d9155c059fe8141bb8aa5900b26dd1
Paul Fitzgerald <paulfitz99@gmail.com>
2018-02-05 20:54:46 +0000

    use functional component

--- components/TopScorers.js
+++ components/TopScorers.js
@@ -12,105 +12,32 @@ const players = [
 		yellowCards: 0,
 		redCards: 0,
 		photo: 'https://dummyimage.com/300'
-	},
-	{
-		name: 'James Cameron',
-		gamesPlayed: 15,
-		goalsScored: 1,
-		yellowCards: 1,
-		redCards: 0,
-		photo: 'https://dummyimage.com/300'
-	},
-	{
-		name: 'Barry Bazzler',
-		gamesPlayed: 15,
-		goalsScored: 0,
-		yellowCards: 0,
-		redCards: 1,
-		photo: 'https://dummyimage.com/300'
-	},
-	{
-		name: 'Ian Nolan',
-		gamesPlayed: 15,
-		goalsScored: 2,
-		yellowCards: 0,
-		redCards: 1,
-		photo: 'https://dummyimage.com/300'
-	},
-	{
-		name: 'Collie McCarthy',
-		gamesPlayed: 16,
-		goalsScored: 5,
-		yellowCards: 1,
-		redCards: 1,
-		photo: 'https://dummyimage.com/300'
-	},
-	{
-		name: 'Steve O Connell',
-		gamesPlayed: 14,
-		goalsScored: 15,
-		yellowCards: 1,
-		redCards: 0,
-		photo: 'https://dummyimage.com/300'
-	},
-	{
-		name: 'Danny Dyer',
-		gamesPlayed: 10,
-		goalsScored: 2,
-		yellowCards: 1,
-		redCards: 0,
-		photo: 'https://dummyimage.com/300'
-	},
-	{
-		name: 'Paul Curtin',
-		gamesPlayed: 5,
-		goalsScored: 9,
-		yellowCards: 1,
-		redCards: 0,
-		photo: 'https://dummyimage.com/300'
-	},
-	{
-		name: 'Gavin Falconer',
-		gamesPlayed: 5,
-		goalsScored: 12,
-		yellowCards: 1,
-		redCards: 2,
-		photo: 'https://dummyimage.com/300'
-	},
-	{
-		name: 'Enda Schifo',
-		gamesPlayed: 5,
-		goalsScored: 1,
-		yellowCards: 1,
-		redCards: 1,
-		photo: 'https://dummyimage.com/300'
-	},
+	}
 ];
 
-class TopScorers extends Component {
-  render() {
+const TopScorers = ({ data }) => {
 
-    const allPlayers = this.props.data.allPlayers || [];
-    return (
-      <ScrollView>
-					<PlayerListHeader>
-						<PlayerNameHeaderText>Name</PlayerNameHeaderText>
-						<HeaderText>GP</HeaderText>
-						<HeaderText>GLS</HeaderText>
-					</PlayerListHeader>
-					{allPlayers.map(player => {
-						return (
-							<PlayerContainer key={player.name}>
-								<PlayerImage source={{uri: player.photo}} />
-								<PlayerName>{player.name}</PlayerName>
-								<PlayerText>{player.gamesPlayed}</PlayerText>
-								<PlayerText>{player.goalsScored}</PlayerText>
-							</PlayerContainer>
-						);
-					})}
-      </ScrollView>
-    );
-  }
+	const allPlayers = data.allPlayers || [];
+
+	return (
+		<ScrollView>
+			<PlayerListHeader>
+				<PlayerNameHeaderText>Name</PlayerNameHeaderText>
+				<HeaderText>GP</HeaderText>
+				<HeaderText>GLS</HeaderText>
+			</PlayerListHeader>
+			{allPlayers.map(player => {
+				return (
+					<PlayerContainer key={player.name}>
+						<PlayerImage source={{uri: player.photo}} />
+						<PlayerName>{player.name}</PlayerName>
+						<PlayerText>{player.gamesPlayed}</PlayerText>
+						<PlayerText>{player.goalsScored}</PlayerText>
+					</PlayerContainer>
+				);
+			})}
+	</ScrollView>
+	)
 }
 
 const PlayerListHeader = styled.View`
