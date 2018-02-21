8ce5d7a4d96e620bf57c6fcd14e99a5aa71ef115
Paul Fitzgerald <paulfitz99@gmail.com>
2018-02-21 08:09:10 +0000

    add facebook login

--- components/HomeScreen.js
+++ components/HomeScreen.js
@@ -1,5 +1,10 @@
 import React, { Component } from 'react';
 import styled from 'styled-components/native';
+const FBSDK = require('react-native-fbsdk');
+const {
+  LoginButton,
+  AccessToken
+} = FBSDK;
 
 import {
 	FlatList,
@@ -39,6 +44,27 @@ const HomeScreen = ({ navigation }) => {
         </TouchableHighlight>
 
       </NavView>
+
+			<View>
+        <LoginButton
+          publishPermissions={["publish_actions"]}
+          onLoginFinished={
+            (error, result) => {
+              if (error) {
+                alert("login has error: " + result.error);
+              } else if (result.isCancelled) {
+                alert("login is cancelled.");
+              } else {
+                AccessToken.getCurrentAccessToken().then(
+                  (data) => {
+                    alert(data.accessToken.toString())
+                  }
+                )
+              }
+            }
+          }
+          onLogoutFinished={() => alert("logout.")}/>
+      </View>
     </View>
   );
 };
